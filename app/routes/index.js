5d18626083ae1be3ec6ea077206f6ed19896fdf2
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-10 11:15:39 -0700

    created answer model.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+  var answers = [{
+    id: 1,
+    author: "DJroychoudhury",
+    content: "This is a test answer. This answer should only show up for this one question."
+  }, {
+    id: 2,
+    author: "Some Body",
+    content: "This is another test answer. should only show up for this other answer."
+  }, {
+    id: 3,
+    author: "Another Body",
+    content: "Another test answer. Should show up alone with this question."
+  }];
+  export default Ember.Route.extend({
+    model() {
+      return answers;
+    },
 });
