2e441424159534c35de51dc65119d0ae1d53bf2d
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-14 10:55:31 -0700

    add update functionality to answers.

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,16 @@ import Ember from 'ember';
         this.transitionTo('index');
       },
 
+      update(answer, params) {
+        Object.keys(params).forEach(function(key) {
+          if(params[key]!==undefined) {
+            answer.set(key,params[key]);
+          }
+        });
+        answer.save();
+        this.transitionTo('index');
+      },
+
       destroyAnswer(answer) {
         answer.destroyRecord();
         this.transitionTo('index');
