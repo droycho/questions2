d73f0e1a2fc2776972c9828eaf9bc90c290d95b5
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-14 19:36:31 -0700

    problem with routes/question.js, line 10. for some reason, it thinks 'question' is undefined. This may be the only thing keeping me from MVP.

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,13 +4,14 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+  //problem here is that 'question' is not being defined for some reason, no neither .get() nor .addObject() are able to be read.
   actions: {
     save3(params) {
       var newAnswer = this.store.createRecord('answer', params);
       var question = params.question;
       question.get('answers').addObject(newAnswer);
       newAnswer.save().then(function() {
-        return question.save();
+        return answer.save();
       });
       this.transitionTo('question', params.question);
     }
