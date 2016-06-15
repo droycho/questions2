f585f70736f24a6dd5c878221a328f5fb7a26e27
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-14 19:51:56 -0700

    corrected spelling error in commect.

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
-  //problem here is that 'question' is not being defined for some reason, no neither .get() nor .addObject() are able to be read.
+  //problem here is that 'question' is not being defined for some reason, so neither .get() nor .addObject() are able to be read.
   actions: {
     save3(params) {
       var newAnswer = this.store.createRecord('answer', params);
