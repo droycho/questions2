6d43526674539d6e1c0f7addefd963ad2898bf9b
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-23 11:53:59 -0700

    add totalAnswers computed proprty, but it is not properly functioning. ember build error states 'question' is not defined in the question-tile.js component. Need to get this defined so that the computed property knows which question to look into.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  totalAnswers: Ember.computed(question.get('answers').get('length'), function() {
+    return this.get(question.get('answers').get('length'));
+  }),
+
   actions: {
     destroyQuestion(question) {
       if (confirm('Are you sure you want to destroy this question?')) {
