98ae16c5d5005a6043afafb45deb3568b8869a62
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-23 08:53:09 -0700

    answers are now saving to firebase. need to get them to display now.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    destroyQuestion(question) {
+      if (confirm('Are you sure you want to destroy this question?')) {
+        this.sendAction('destroyQuestion', question);
+      }
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,14 +4,14 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
-  //problem here is that 'question' is not being defined for some reason, so neither .get() nor .addObject() are able to be read.
+  //error in console says 'Uncaught TypeError: Cannot read property 'get of undefined' -- meaning question is returning as undefined on save, problem with routes?
   actions: {
     save3(params) {
       var newAnswer = this.store.createRecord('answer', params);
       var question = params.question;
       question.get('answers').addObject(newAnswer);
       newAnswer.save().then(function() {
-        return answer.save();
+        return question.save(); 
       });
       this.transitionTo('question', params.question);
     }
