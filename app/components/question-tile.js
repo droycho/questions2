dda4f8f4435e31b3c7f8819811e97566bdca8dd0
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-23 13:57:41 -0700

    fixed service for creating a list of favorite questions.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -5,8 +5,8 @@ export default Ember.Component.extend({
 
 //for can't figure out how to get the computed property of one model property, all examples are for computing new proprty from two or more other properties. just want the total number of answers each question has.
 
-  // totalAnswers: Ember.computed(this.question.answers.length, function() {
-  //   return this.get(this.question.answers.length);
+  // totalAnswers: Ember.computed(question.get('answers').get('length'), function() {
+  //   return this.get(question.get('answers').get('length'));
   // }),
 
   actions: {
@@ -16,8 +16,8 @@ export default Ember.Component.extend({
       }
     },
 
-    addToList(question) {
-      this.get('favoritesList').add(question);
+    addToList(item) {
+      this.get('favoritesList').add(item);
     }
   }
 });
