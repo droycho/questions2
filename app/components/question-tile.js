4f5704af31912cdcd939d746ab6e81bc9e7097d5
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-06-23 17:07:11 -0700

    fixed computed function, MVP passes.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,11 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   favoritesList: Ember.inject.service(),
 
-//for can't figure out how to get the computed property of one model property, all examples are for computing new proprty from two or more other properties. just want the total number of answers each question has.
+//for can't figure out how to get the computed property using one property, all examples are for computing new proprty from two or more other properties. just want the total number of answers each question has.
 
-  // totalAnswers: Ember.computed(question.get('answers').get('length'), function() {
-  //   return this.get(question.get('answers').get('length'));
-  // }),
+  totalAnswers: Ember.computed('question.answers.length', function() {
+    var len = this.get('question.answers.length');
+    console.log(len);
+    return len;
+  }),
 
   actions: {
     destroyQuestion(question) {
